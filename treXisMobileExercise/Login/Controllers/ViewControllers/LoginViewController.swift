3c12949d544bfaec4b4d49eea90847c76aa2d7e5
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2021-08-13 12:44:06 -0600

    Created LoginViewController and made it the rootViewController

---
+++ treXisMobileExercise/Login/Controllers/ViewControllers/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  treXisMobileExercise
+//
+//  Created by Timothy Rosenvall on 8/13/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    //MARK: - Constants and Variables
+    
+    //MARK: - Objects and IBOutlets
+    let usernameTextField = UITextField()
+    let passwordTextField = UITextField()
+    let loginButton = UIButton()
+    
+    //MARK: - Constraints
+    
+    //MARK: - Lifecycle Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        setupViews()
+    }
+    
+    //MARK: - Setup and Constraint Functions
+    func setupViews() {
+    }
+    
+    //MARK: - UI Functions and IBActions
+    
+    //MARK: - Helper Functions
+    
+}
---
+++ treXisMobileExercise/Resources/StyleGuide.swift
@@ -0,0 +1,20 @@
+//
+//  StyleGuide.swift
+//  treXisMobileExercise
+//
+//  Created by Timothy Rosenvall on 8/13/21.
+//
+
+import UIKit
+
+class StyleGuide {
+    //Color Guide
+    static let primaryColor = #colorLiteral(red: 0.7769108415, green: 0.5924536586, blue: 0.9964304566, alpha: 1)
+    static let secondaryColor = #colorLiteral(red: 0.1395053864, green: 1, blue: 0.7252087593, alpha: 1)
+    static let tertiaryColor = #colorLiteral(red: 1, green: 0.9318215251, blue: 0.6035651565, alpha: 1)
+    
+    //Font Size Guide
+    static let titleFontSize: CGFloat = 40
+    static let subtitleFontSize: CGFloat = 40 * 0.618
+    static let bodyFontSize: CGFloat = 40 * 0.618 ** 2
+}
