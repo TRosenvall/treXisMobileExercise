480bb92202dbaf5cf27d87de945700c6c9852b95
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2021-08-14 16:34:04 -0600

    Built UI for the LoginViewController

--- treXisMobileExercise/Login/Controllers/ViewControllers/LoginViewController.swift
+++ treXisMobileExercise/Login/Controllers/ViewControllers/LoginViewController.swift
@@ -13,24 +13,122 @@ class LoginViewController: UIViewController {
     
     //MARK: - Objects and IBOutlets
     let usernameTextField = UITextField()
+    let usernameUnderlineView = UIView()
     let passwordTextField = UITextField()
-    let loginButton = UIButton()
-    
-    //MARK: - Constraints
-    
+    let passwordUnderlineView = UIView()
+    let loginButton = UIButton(type: .system)
+
     //MARK: - Lifecycle Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = .white
         setupViews()
     }
     
     //MARK: - Setup and Constraint Functions
     func setupViews() {
+        setupSelfView()
+        setupPasswordTextField()
+        setupPasswordUnderlineView()
+        setupUsernameTextField()
+        setupUsernameUnderlineView()
+        setupLoginButton()
+    }
+    
+    func setupSelfView() {
+        self.view.backgroundColor = StyleGuide.primaryColor
+    }
+    
+    func setupPasswordTextField() {
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(passwordTextField)
+        
+        passwordTextField.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        passwordTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        passwordTextField.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: StyleGuide.ratio ** 0.5).isActive = true
+        passwordTextField.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: StyleGuide.ratio ** 6).isActive = true
+        
+        passwordTextField.backgroundColor = StyleGuide.primaryColor
+        passwordTextField.textColor = StyleGuide.accentColorOne
+        
+        //Adding Text Padding
+        passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: passwordTextField.frame.height))
+        passwordTextField.leftViewMode = .always
+        
+        passwordTextField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: passwordTextField.frame.height))
+        passwordTextField.rightViewMode = .always
+        
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password",
+                                                                     attributes: [NSAttributedString.Key.foregroundColor: StyleGuide.accentColorOne])
+    }
+    
+    func setupPasswordUnderlineView() {
+        passwordUnderlineView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(passwordUnderlineView)
+
+        passwordUnderlineView.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: -8).isActive = true   //TODO: - Find way to adject based on NSLayoutConstraint values
+        passwordUnderlineView.bottomAnchor.constraint(equalTo: passwordUnderlineView.topAnchor, constant: 1).isActive = true
+        passwordUnderlineView.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor).isActive = true
+        passwordUnderlineView.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor).isActive = true
+
+        passwordUnderlineView.backgroundColor = StyleGuide.accentColorOne
+    }
+    
+    func setupUsernameTextField() {
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(usernameTextField)
+        
+        usernameTextField.bottomAnchor.constraint(equalTo: passwordTextField.topAnchor).isActive = true
+        usernameTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        usernameTextField.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: StyleGuide.ratio ** 0.5).isActive = true
+        usernameTextField.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: StyleGuide.ratio ** 6).isActive = true
+        
+        usernameTextField.backgroundColor = StyleGuide.primaryColor
+        usernameTextField.textColor = StyleGuide.accentColorOne
+        
+        //Adding Text Padding
+        usernameTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: usernameTextField.frame.height))
+        usernameTextField.leftViewMode = .always
+        
+        usernameTextField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: usernameTextField.frame.height))
+        usernameTextField.rightViewMode = .always
+        
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: "Username",
+                                                                     attributes: [NSAttributedString.Key.foregroundColor: StyleGuide.accentColorOne])
+    }
+    
+    func setupUsernameUnderlineView() {
+        usernameUnderlineView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(usernameUnderlineView)
+
+        usernameUnderlineView.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: -8).isActive = true   //TODO: - Find way to adject based on NSLayoutConstraint values
+        usernameUnderlineView.bottomAnchor.constraint(equalTo: usernameUnderlineView.topAnchor, constant: 1).isActive = true
+        usernameUnderlineView.leadingAnchor.constraint(equalTo: usernameTextField.leadingAnchor).isActive = true
+        usernameUnderlineView.trailingAnchor.constraint(equalTo: usernameTextField.trailingAnchor).isActive = true
+
+        usernameUnderlineView.backgroundColor = StyleGuide.accentColorOne
+    }
+    
+    func setupLoginButton() {
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(loginButton)
+
+        loginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 8).isActive = true
+        loginButton.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: StyleGuide.ratio ** 0.5).isActive = true
+        loginButton.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: StyleGuide.ratio ** 6.5).isActive = true
+
+        loginButton.layer.cornerRadius = 8  //TODO: - Find way to adject based on NSLayoutConstraint values
+        loginButton.backgroundColor = StyleGuide.accentColorThree
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.setTitleColor(StyleGuide.primaryColor, for: .normal)
+        
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
     }
     
     //MARK: - UI Functions and IBActions
+    @objc func loginButtonTapped() {
+        print("loginButtonTapped")
+    }
     
     //MARK: - Helper Functions
     
--- treXisMobileExercise/Resources/StyleGuide.swift
+++ treXisMobileExercise/Resources/StyleGuide.swift
@@ -8,13 +8,11 @@
 import UIKit
 
 class StyleGuide {
-    //Color Guide
-    static let primaryColor = #colorLiteral(red: 0.7769108415, green: 0.5924536586, blue: 0.9964304566, alpha: 1)
-    static let secondaryColor = #colorLiteral(red: 0.1395053864, green: 1, blue: 0.7252087593, alpha: 1)
-    static let tertiaryColor = #colorLiteral(red: 1, green: 0.9318215251, blue: 0.6035651565, alpha: 1)
+    static let ratio: CGFloat = 0.618 //Golden Ratio, Used for resizing elements
     
-    //Font Size Guide
-    static let titleFontSize: CGFloat = 40
-    static let subtitleFontSize: CGFloat = 40 * 0.618
-    static let bodyFontSize: CGFloat = 40 * 0.618 ** 2
+    //Color Guide
+    static let primaryColor: UIColor = #colorLiteral(red: 0.184291482, green: 0.1843293309, blue: 0.184286505, alpha: 1)
+    static let accentColorOne: UIColor = #colorLiteral(red: 0.8754730225, green: 0.8788470626, blue: 0.9136125445, alpha: 1)
+    static let accentColorTwo: UIColor = #colorLiteral(red: 1, green: 0.2400430441, blue: 0.3274297714, alpha: 1)
+    static let accentColorThree: UIColor = #colorLiteral(red: 0.3864992857, green: 1, blue: 0.6309787631, alpha: 1)
 }
