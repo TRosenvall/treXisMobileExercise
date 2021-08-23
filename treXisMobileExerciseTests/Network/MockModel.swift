48e9da4194af18d1f663df0ecb81f9fb45e14cac
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2021-08-22 21:17:56 -0600

    Finished NetworkRequestTests unit testing

---
+++ treXisMobileExerciseTests/Network/MockModel.swift
@@ -0,0 +1,15 @@
+//
+//  MockModel.swift
+//  treXisMobileExerciseTests
+//
+//  Created by Timothy Rosenvall on 8/22/21.
+//
+
+import Foundation
+
+struct MockModel: Equatable, Codable
+{
+    let id: String
+    let name: String
+    let balance: Float
+}
